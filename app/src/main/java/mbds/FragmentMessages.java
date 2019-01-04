c6ee9b491217186482ff1b246b75bac64ed00e67
SergioSim <sergio.simonian@etu.unice.fr>
2019-01-04 15:01:10 +0100

    adding UI elements to fragment messages

--- app/src/main/java/mbds/FragmentMessages.java
+++ app/src/main/java/mbds/FragmentMessages.java
@@ -5,12 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 import mbdse.R;
 
 public class FragmentMessages extends Fragment {
     private String txt;
     private TextView tv;
+    private EditText textInput;
 
     public FragmentMessages(){}
 
@@ -24,6 +26,7 @@ public class FragmentMessages extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_messages, container, false);
         tv = v.findViewById(R.id.idTxt);
+        textInput = v.findViewById(R.id.messageText);
         return v;
     }
 
